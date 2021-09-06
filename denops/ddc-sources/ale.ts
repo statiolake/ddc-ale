9c76213df1b70aaaaa6030cce6132ac737668af3
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2021-09-05 19:58:06 -0700

    Refactor code to use refresh_candidates
    I don't love this code in that I think it's a bit more complex to
    follow, it is a big win in terms of performance because it unblocks
    other ddc sources from also returning and gets initial result showing up
    right away

--- denops/ddc-sources/ale.ts
+++ denops/ddc-sources/ale.ts
@@ -3,42 +3,59 @@
 import {
   BaseSource,
   Candidate,
-} from "https://deno.land/x/ddc_vim@v0.4.3/types.ts";
+} from "https://deno.land/x/ddc_vim@v0.5.2/types.ts";
+import { batch, vars } from "https://deno.land/x/ddc_vim@v0.5.2/deps.ts";
 import {
   GatherCandidatesArguments,
-  GetCompletePositionArguments,
-} from "https://deno.land/x/ddc_vim@v0.4.3/base/source.ts";
-import { once } from "https://deno.land/x/denops_std@v1.8.1/anonymous/mod.ts";
+  OnInitArguments,
+} from "https://deno.land/x/ddc_vim@v0.5.2/base/source.ts";
+
+const DEFAULT_EMPTY_RESULTS: Candidate[] = [];
 
 export class Source extends BaseSource {
-  getCompletePosition(
-    { denops, context }: GetCompletePositionArguments,
-  ): Promise<number> {
-    return denops.call(
-      "ale#completion#GetCompletionPositionForDeoplete",
-      context.input,
-    ) as Promise<number>;
+  async onInit(args: OnInitArguments): Promise<void> {
+    await batch(args.denops, async (denops) => {
+      await vars.g.set(denops, "ddc#source#ale#_results", DEFAULT_EMPTY_RESULTS);
+      await vars.g.set(denops, "ddc#source#ale#_requested", false);
+      await vars.g.set(denops, "ddc#source#ale#_prev_input", "");
+    });
   }
 
   async gatherCandidates(
-    { denops }: GatherCandidatesArguments,
+    args: GatherCandidatesArguments,
   ): Promise<Candidate[]> {
-    return await new Promise<Candidate[]>((resolve) => {
-      denops.call(
-        "ddc#ale#get_completions",
-        denops.name,
-        once(denops, (results: unknown) => {
-          const candidates: Candidate[] = Array.isArray(results) ? results : [];
-          // FIXME: Hack: Some LSP (such as Rust Analyzer) sometimes returns
-          // candidates ending with whitespace, so fix them here.
-          candidates.forEach(
-            (candidate) => {
-              candidate.word = candidate.word.trimEnd();
-            },
-          );
-          resolve(candidates);
-        })[0],
+    const prevInput: string = await vars.g.get(
+      args.denops,
+      "ddc#source#ale#_prev_input",
+    ).then(p => typeof p === 'string' ? p : '');
+    const requested: boolean = await vars.g.get(
+      args.denops,
+      "ddc#source#ale#_requested",
+    ).then(r => typeof r === 'boolean' ? r : false);
+
+    if (args.context.input === prevInput && requested) {
+      const results: Candidate[] = await vars.g.get(
+        args.denops,
+        "ddc#source#ale#_results",
+      ) ?? DEFAULT_EMPTY_RESULTS;
+      // FIXME: Hack: Some LSPs (such as Rust Analyzer) sometimes return
+      // candidates ending with whitespace, so fix them here.
+      results.forEach((result) => result.word = result.word.trimEnd());
+      return results;
+    }
+
+    await batch(args.denops, (denops) => {
+      vars.g.set(denops, "ddc#source#ale#_results", []);
+      vars.g.set(denops, "ddc#source#ale#_requested", false);
+      vars.g.set(
+        denops,
+        "ddc#source#ale#_prev_input",
+        args.context.input,
       );
+      denops.call("ddc#ale#get_completions");
+      return Promise.resolve();
     });
+
+    return [];
   }
 }
